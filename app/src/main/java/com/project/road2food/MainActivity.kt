076613399d22cfaeb146b413a02a510c8ee5b172
saneprkl <santeri.lehtonen@gmail.com>
2022-04-01 12:06:31 +0300

    added navbar to this branch

--- app/src/main/java/com/project/road2food/MainActivity.kt
+++ app/src/main/java/com/project/road2food/MainActivity.kt
@@ -3,18 +3,22 @@ package com.project.road2food
 //import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.user_login.*
 import kotlinx.android.synthetic.main.user_login.registration
 import kotlinx.android.synthetic.main.user_registeration.*
 import kotlinx.android.synthetic.main.account.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
+//import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val bottomNavigationView = supportFragmentManager
 
         registration.setOnClickListener{
             showRegistration()
@@ -27,6 +31,35 @@ class MainActivity : AppCompatActivity() {
         login.setOnClickListener{
             showAccount()
         }
+
+        val navView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        navView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    Toast.makeText(this, "Home selected", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.nav_account -> {
+                    Toast.makeText(this, "Photos selected", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.nav_map -> {
+                    Toast.makeText(this, "More selected", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.nav_offers -> {
+                    Toast.makeText(this, "More selected", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> true
+            }
+        }
+
+
+
+
+
+
     }
 private fun showRegistration(){
     registration_layout.visibility= View.VISIBLE
